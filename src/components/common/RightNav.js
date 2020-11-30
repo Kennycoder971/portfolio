23fc57ff5ecf73760bbe01da2ba8652ee10351e6
Kenny Delver <kennycoder971@gmail.com>
2020-11-30 20:22:54 +0100

    adding google analytics

--- src/components/common/RightNav.js
+++ src/components/common/RightNav.js
@@ -4,7 +4,7 @@ import { MdClose } from "react-icons/md";
 
 export default function RightNav({ isNavOpen, setIsNavOpen }) {
   return (
-    <nav className={`right-nav ${isNavOpen && "active"}`}>
+    <nav className={`right-nav ${isNavOpen ? "active" : ""}`}>
       <ul className="right-nav-menu">
         <li className="right-nav-menu-link">
           <NavLink to="/" onClick={() => setIsNavOpen(false)}>
